606c0a9e92f539c9ae040e8a0b34775f375c0dc2
Christopher Grote <chris@thegrotes.net>
2015-01-07 21:06:18 +0000

    Initial version of working Dailymile sync (at least upload to and basic download)

--- tapiriik/services/Dailymile/dailymile.py
+++ tapiriik/services/Dailymile/dailymile.py
@@ -16,6 +16,7 @@ import logging
 import pytz
 import re
 import time
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -74,7 +75,15 @@ class DailymileService(ServiceBase):
            "https://api.dailymile.com/oauth/authorize?" + urlencode(params)
 
     def _apiHeaders(self, serviceRecord):
-        return {"Authorization": "access_token " + serviceRecord.Authorization["OAuthToken"]}
+        return {"Authorization": "access_token " + serviceRecord.Authorization["oauth_token"]}
+
+    def _paramsIncludingAuth(self, params, serviceRecord):
+        params["oauth_token"] = serviceRecord.Authorization["oauth_token"]
+        return params
+
+    # Yes, this is pathetic, but my Python is this rusty!
+    def _getDateFmt(self):
+        return "%Y-%m-%dT%H:%M:%SZ"
 
     def RetrieveAuthorizationToken(self, req, level):
         code = req.GET.get("code")
@@ -86,11 +95,9 @@ class DailymileService(ServiceBase):
             raise APIException("Invalid code")
         data = response.json()
 
-#        authorizationData = {"OAuthToken": data["access_token"]}
         authorizationData = {"oauth_token": data["access_token"]}
         # Retrieve the user ID, meh.
         self._globalRateLimit()
-#        id_resp = requests.get("https://api.dailymile.com/people/me.json", headers=self._apiHeaders(ServiceRecord({"Authorization": authorizationData})))
         id_resp = requests.get("https://api.dailymile.com/people/me.json", data=authorizationData)
         if response.status_code != 200:
             raise APIException("Unable to retrieve username: " + str(response.status_code))
@@ -105,16 +112,15 @@ class DailymileService(ServiceBase):
         activities = []
         exclusions = []
         before = earliestDate = None
+        #        page = 1 # ... only option seems to be the "page" parameter, with 1 being the most recent 20 activities (fixed size), and working backwards from there
 
         while True:
             if before is not None and before < 0:
                 break # Caused by activities that "happened" before the epoch. We generally don't care about those activities...
             logger.debug("Req with before=" + str(before) + "/" + str(earliestDate))
             self._globalRateLimit()
-            # TODO: not entirely sure what "before" represents
-            # -- assuming here it's equivalent to "since" in Dailymile API ("Fetch all entries with a timestamp greater than 'since'")
-            #    though in that case it is important that 'before' holds a UNIX timestamp (probably UTC)!  (not sure if it does?)
-            resp = requests.get("https://api.dailymile.com/people/" + str(svcRecord.ExternalID) + "/entries.json", headers=self._apiHeaders(svcRecord), params={"since": before})
+            resp = requests.get("https://api.dailymile.com/people/" + str(svcRecord.ExternalID) + "/entries.json", headers=self._apiHeaders(svcRecord), params={"until": before})
+#            resp = requests.get("https://api.dailymile.com/people/" + str(svcRecord.ExternalID) + "/entries.json", params=self._paramsIncludingAuth({"page": page}, svcRecord))
             if resp.status_code == 401:
                 raise APIException("No authorization to retrieve activity list", block=True, user_exception=UserException(UserExceptionType.Authorization, intervention_required=True))
 
@@ -124,72 +130,75 @@ class DailymileService(ServiceBase):
 
             if not len(reqdata):
                 break  # No more activities to see
+            elif not len(reqdata["entries"]):
+                break  # Same as above -- could have sworn I once saw just an empty array, with no 'entries', so leaving both
 
-            for ride in reqdata:
-                activity = UploadedActivity()
-                # From the rest of the API, looks like Dailymile probably standardizes on UTC (creating new entries expects them to use UTC, and no timezone otherwise available from downloading activities)
-                activity.TZ = pytz.UTC
-                activity.StartTime = pytz.utc.localize(datetime.strptime(ride["at"], "%Y-%m-%dT%H:%M:%SZ"))
-                logger.debug("\tActivity s/t %s: %s" % (activity.StartTime, ride["workout"]["title"]))
-                if not earliestDate or activity.StartTime < earliestDate:
-                    earliestDate = activity.StartTime
-                    before = calendar.timegm(activity.StartTime.astimezone(pytz.utc).timetuple())
+            # TODO: this does not seem to be looping properly -- it's only picking up the first entry, then breaking out
+            for ride in reqdata["entries"]:
+                
+                # Because Dailymile can also have media entries (just photos, no workout data), we need to ensure
+                # this is actually some kind of fitness activity before we proceed processing the data
+                if ('workout' in ride):
+                
+                    activity = UploadedActivity()
+                    # From the rest of the API, looks like Dailymile probably standardizes on UTC (creating new entries expects them to use UTC, and no timezone otherwise available from downloading activities)
+                    activity.TZ = pytz.UTC
+                    # Pretty sure what's recorded on Dailymile is the completion time (time of posting)
+                    activity.EndTime = pytz.utc.localize(datetime.strptime(ride["at"], self._getDateFmt()))
+                    #                    activity.StartTime = pytz.utc.localize(datetime.strptime(ride["at"], self._getDateFmt()))
+                    logger.debug("\tActivity e/t %s: %s" % (activity.EndTime, ride["workout"]["title"]))
+                    if not earliestDate or activity.EndTime < earliestDate:
+                        earliestDate = activity.EndTime
+                        before = calendar.timegm(activity.EndTime.astimezone(pytz.utc).timetuple())
+                    
+                    # Duration isn't mandatory on Dailymile, and may be missing
+                    if ('duration' in ride["workout"]):
+                        # Hope 'timedelta' assumes we just send it an elapsed time / duration (in seconds) of the activity (?)
+                        activity.StartTime = activity.EndTime - timedelta(0, ride["workout"]["duration"])
 
-                # Hope 'timedelta' assumes we just send it an elapsed time / duration (in seconds) of the activity (?)
-                activity.EndTime = activity.StartTime + timedelta(0, ride["workout"]["duration"])
-                # No indication in Dailymile whether an activity is manually-created or uploaded
-                activity.ServiceData = {"ActivityID": ride["id"]}
+                    activity.ServiceData = {"ActivityID": ride["id"]}
 
-                if ride["workout"]["activity_type"] not in self._reverseActivityTypeMappings:
-                    exclusions.append(APIExcludeActivity("Unsupported activity type %s" % ride["workout"]["activity_type"], activity_id=ride["id"], user_exception=UserException(UserExceptionType.Other)))
-                    logger.debug("\t\tUnknown activity")
-                    continue
+                    if ride["workout"]["activity_type"] not in self._reverseActivityTypeMappings:
+                        exclusions.append(APIExcludeActivity("Unsupported activity type %s" % ride["workout"]["activity_type"], activity_id=ride["id"], user_exception=UserException(UserExceptionType.Other)))
+                        logger.debug("\t\tUnknown activity")
+                        continue
 
-                activity.Type = self._reverseActivityTypeMappings[ride["workout"]["activity_type"]]
+                    activity.Type = self._reverseActivityTypeMappings[ride["workout"]["activity_type"]]
 
-                # Distance returned is as the user entered it...
-                distunit = ride["workout"]["distance"]["units"]
-                distmeas = ride["workout"]["distance"]["value"]
+                    # Don't think distance is mandatory on Dailymile, either...
+                    if ('distance' in ride["workout"]):
+                        # Distance returned is as the user entered it...
+                        distunit = ride["workout"]["distance"]["units"]
+                        distmeas = ride["workout"]["distance"]["value"]
 
-                if (distunit == "miles"):
-                    activity.Stats.Distance = ActivityStatistic(ActivityStatisticUnit.Miles, value=distmeas)
-                elif (distunit == "yards"):
-                    activity.Stats.Distance = ActivityStatistic(ActivityStatisticUnit.Miles, value=(distmeas/1760))
-                elif (distunit == "meters"):
-                    activity.Stats.Distance = ActivityStatistic(ActivityStatisticUnit.Meters, value=distmeas)
-                elif (distunit == "kilometers"):
-                    activity.Stats.Distance = ActivityStatistic(ActivityStatisticUnit.Kilometers, value=distmeas)
-                else:
-                    exclusions.append(APIExcludeActivity("Unsupported distance unit %s" % distunit, activity_id=ride["id"], user_exception=UserException(UserException.Other)))
-                    logger.debug("\t\tUnknown measurement unit")
-                    continue
+                        if (distunit == "miles"):
+                            activity.Stats.Distance = ActivityStatistic(ActivityStatisticUnit.Miles, value=distmeas)
+                        elif (distunit == "yards"):
+                            activity.Stats.Distance = ActivityStatistic(ActivityStatisticUnit.Miles, value=(distmeas/1760))
+                        elif (distunit == "meters"):
+                            activity.Stats.Distance = ActivityStatistic(ActivityStatisticUnit.Meters, value=distmeas)
+                        elif (distunit == "kilometers"):
+                            activity.Stats.Distance = ActivityStatistic(ActivityStatisticUnit.Kilometers, value=distmeas)
+                        else:
+                            exclusions.append(APIExcludeActivity("Unsupported distance unit %s" % distunit, activity_id=ride["id"], user_exception=UserException(UserException.Other)))
+                            logger.debug("\t\tUnknown measurement unit")
+                            continue
 
-                # Dailymile automatically calculates average speed (and calories), and doesn't seem to support max speed, moving time, or "timer time"
-                # and while they let you enter avg and max heart rates, the API does not expose them
+                    # Dailymile automatically calculates average speed (and calories), and doesn't seem to support max speed, moving time, or "timer time"
+                    # and while they let you enter avg and max heart rates, and upload GPS tracks, the API does not expose them on existing activities
 
-#                if "max_speed" in ride or "average_speed" in ride:
-#                    activity.Stats.Speed = ActivityStatistic(ActivityStatisticUnit.MetersPerSecond, avg=ride["average_speed"] if "average_speed" in ride else None, max=ride["max_speed"] if "max_speed" in ride else None)
-#                activity.Stats.MovingTime = ActivityStatistic(ActivityStatisticUnit.Seconds, value=ride["moving_time"] if "moving_time" in ride and ride["moving_time"] > 0 else None)  # They don't let you manually enter this, and I think it returns 0 for those activities.
-
-#                if "average_watts" in ride:
-#                    activity.Stats.Power = ActivityStatistic(ActivityStatisticUnit.Watts, avg=ride["average_watts"])
-#                if "average_heartrate" in ride:
-#                    activity.Stats.HR.update(ActivityStatistic(ActivityStatisticUnit.BeatsPerMinute, avg=ride["average_heartrate"]))
-#                if "max_heartrate" in ride:
-#                    activity.Stats.HR.update(ActivityStatistic(ActivityStatisticUnit.BeatsPerMinute, max=ride["max_heartrate"]))
-#                if "average_cadence" in ride:
-#                    activity.Stats.Cadence.update(ActivityStatistic(ActivityStatisticUnit.RevolutionsPerMinute, avg=ride["average_cadence"]))
-#                if "average_temp" in ride:
-#                    activity.Stats.Temperature.update(ActivityStatistic(ActivityStatisticUnit.DegreesCelcius, avg=ride["average_temp"]))
-#                if "calories" in ride:
-#                    activity.Stats.Energy = ActivityStatistic(ActivityStatisticUnit.Kilocalories, value=ride["calories"])
-                activity.Name = ride["workout"]["title"]
-#                activity.Private = ride["private"]
-#                activity.Stationary = ride["manual"]
-#                activity.GPS = ("start_latlng" in ride) and (ride["start_latlng"] is not None)
-                activity.AdjustTZ()
-                activity.CalculateUID()
-                activities.append(activity)
+                    # Even title is not mandatory for a workout
+                    if ('title' in ride["workout"]):
+                        activity.Name = ride["workout"]["title"]
+                    elif ('message' in ride):
+                        activity.Name = ride["message"]  # assuming people might enter into Message instead of title (?)
+                    else:
+                        activity.Name = activity.Type  # otherwise resort to just using the activity type (one of the only things that is mandatory)
+                    activity.Private = False
+                    activity.GPS = False
+                    activity.AdjustTZ()
+                    activity.CalculateUID()
+                    activities.append(activity)
 
             if not exhaustive or not earliestDate:
                 break
@@ -197,8 +206,7 @@ class DailymileService(ServiceBase):
         return activities, exclusions
 
     # TODO: For now, information retrieved will be very basic (no GPS, etc)
-    # URL included below to access individual entries: maybe some further experimentation will reveal that things like HR, GPS data, etc are available this way (?)
-    # Unfortunately the API documentation is sparse...
+    # URL included below to access individual entries: unfortunately experimenting with this does not seem to reveal any additional data (GPS, HR, etc all still missing)
     def DownloadActivity(self, svcRecord, activity):
         # We've probably got as much information as we're going to get - we need to copy it into a Lap though.
         activity.Laps = [Lap(startTime=activity.StartTime, endTime=activity.EndTime, stats=activity.Stats)]
@@ -234,23 +242,36 @@ class DailymileService(ServiceBase):
 
         upload_id = None
         
-        # TODO: need to set units according to user preference (not hard-coded)
-        req = { "message": activity.Notes }
+        # Dailymile API says "message" is optional, but leaving it out (or blank) returns a 422 status saying it can't be blank...
+        req = {}
+        if activity.Notes is not None:
+            req["message"] = activity.Notes
+        #        else:
+        #            req["message"] = activity.Name if activity.Name else activity.Type
         req["workout"] = {
                             "title": activity.Name if activity.Name else activity.Type,
                             "activity_type": self._activityTypeMappings[activity.Type],
                             "duration": round((activity.EndTime - activity.StartTime).total_seconds()),
-                            "completed_at": activity.EndTime.astimezone('UTC').strftime("%Y-%m-%dT%H:%M:%SZ")
+                            "completed_at": activity.EndTime.astimezone(pytz.utc).strftime(self._getDateFmt())
                         }
-        req["workout"]["distance"] = {
-                                        "value": activity.Stats.Distance.asUnits(ActivityStatisticUnit.Kilometers).Value,
-                                        "units": "kilometers"
-                                    }
+
+        # TODO: need to set units according to user preference (not hard-coded)
+        # Swimming is the only activity type that does not accept "km" as a valid unit, and requires meters or yards
+        if(activity.Type == "Swimming"):
+            req["workout"]["distance"] = {
+                                            "value": activity.Stats.Distance.asUnits(ActivityStatisticUnit.Meters).Value,
+                                            "units": "meters"
+                                        }
+        else:
+            req["workout"]["distance"] = {
+                                            "value": activity.Stats.Distance.asUnits(ActivityStatisticUnit.Kilometers).Value,
+                                            "units": "kilometers"
+                                        }
         
-        headers = self._apiHeaders(serviceRecord)
+        params = self._paramsIncludingAuth({}, serviceRecord)
         self._globalRateLimit()
 
-        response = requests.post("https://api.dailymile.com/entries.json", data=json.dumps(req), headers=headers)
+        response = requests.post("https://api.dailymile.com/entries.json", params=params, data=json.dumps(req), headers={"Content-Type": "application/json"})
 
         if response.status_code != 201:
             if response.status_code == 401:
@@ -278,18 +299,16 @@ class DailymileService(ServiceBase):
             self._globalRateLimit()
 
             # Add Content-Type to the headers
-            headers["Content-Type"] = "application/gpx+xml"
+            headers = {"Content-Type": "application/gpx+xml"}
 
-#            with open(gpxData, 'rb') as f:     # shouldn't be needed, as gpxData is a string (not a file) (?)
-            response = requests.put("https://api.dailymile.com/entries/" + str(upload_id) + "/track.json", headers=headers, data=gpxData)
-            logger.debug("Waiting for upload - status %s id %s" % (response.json()["status"], response.json()["activity_id"]))
-            if response.json()["error"]:
-                error = response.json()["error"]
-                if "duplicate of activity" in error:
+            response = requests.put("https://api.dailymile.com/entries/" + str(upload_id) + "/track.json", headers=headers, params=params, data=gpxData)
+            if response.status_code != 201:
+                logger.info("Problem uploading GPX of activity for ID: " + str(upload_id) + " - response: " + str(response.text))
+                if "duplicate of activity" in response.text:
                     self.LastUpload = datetime.now()
                     logger.debug("Duplicate")
                     return # I guess we're done here?
-                raise APIException("Dailymile failed while processing activity - last status %s" % response.text)
+                raise APIException("Dailymile failed while uploading GPX of activity - last status %s" % response.text)
             upload_poll_wait = min(30, upload_poll_wait * 2)
             
         self.LastUpload = datetime.now()
